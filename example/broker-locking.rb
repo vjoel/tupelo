bf9faff782c3621b350973612f739b40913e47b9
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-07-23 16:21:03 -0700

    added two examples of broker with and without locking

---
+++ example/broker-locking.rb
@@ -0,0 +1,43 @@
+# Clients attempt to pair up, using a distributed broker algorithm and a lock
+# tuple.
+
+require 'tupelo/app'
+
+N_PLAYERS = 10
+
+token = ["token"] # only the holder of the token can arrange games
+
+Tupelo.application do |app|
+  app.local do |client|
+    client.write token
+  end
+  
+  N_PLAYERS.times do
+    app.child do |client|
+      me = client.client_id
+
+      client.take token # bottleneck and fragile until 'client.write token'
+      other_player = client.read_nowait(name: nil)
+        # sleep 1 # program takes ~N_PLAYERS sec to finish
+
+      if other_player
+        client.take other_player
+        client.write(
+          player1: me,
+          player2: other_player["name"])
+        client.write token
+        you = other_player["name"]
+
+      else
+        client.write(name: me)
+        client.write token
+        game = client.read(
+          player1: nil,
+          player2: me)
+        you = game["player1"]
+      end
+
+      client.log "now playing with #{you}"
+    end
+  end
+end
---
+++ example/broker-optimistic.rb
@@ -0,0 +1,41 @@
+# The local control flow in this example is more complex than in
+# broker-locking.rb, but it has far fewer bottlenecks (try inserting some sleep
+# 1 calls), and it is not possible for a token to be lost (leaving the lock in a
+# locked state) if a process dies.
+
+require 'tupelo/app'
+
+N_PLAYERS = 10
+
+Tupelo.application do |app|
+  N_PLAYERS.times do
+    app.child do |client|
+      me = client.client_id
+      client.write name: me
+      you = nil
+      
+      1.times do
+        begin
+          t = client.transaction
+          if t.take_nowait name: me
+            you = t.take(name: nil)["name"]
+            t.write(
+              player1: me,
+              player2: you)
+            t.commit.wait
+            break
+          end
+        rescue Tupelo::Client::TransactionFailure => ex
+        end
+          
+        game = client.read_nowait(
+          player1: nil,
+          player2: me)
+        redo unless game
+        you = game["player1"]
+      end
+
+      client.log "now playing with #{you}"
+    end
+  end
+end
