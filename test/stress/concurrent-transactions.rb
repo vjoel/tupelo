e004ba9ff0e86a8a73be4f64c960b6e7ba769f0c
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-07-26 15:00:52 -0700

    stress test uses time fuzzing

--- test/stress/concurrent-transactions.rb
+++ test/stress/concurrent-transactions.rb
@@ -1,31 +1,37 @@
 require 'tupelo/app'
+require 'tupelo/app/monitor'
+require 'time-fuzz'
 
 N = 100
+VERBOSE = ARGV.delete "-v"
+
+client_class = Tupelo::TimeFuzz::Client
+Tupelo::TimeFuzz.sleep_max = 0.01
 
 Tupelo.application do |app|
-  app.child do |client|
+  app.start_monitor if VERBOSE
+
+  app.child(client_class) do |client|
     N.times do
       client.transaction do |t|
         x, y = t.take [nil, nil]
-        sleep rand/100
         t.write [x+1, y]
       end
     end
     client.write ["done"]
   end
 
-  app.child do |client|
+  app.child(client_class) do |client|
     N.times do
       client.transaction do |t|
         x, y = t.take [nil, nil]
-        sleep rand/100
         t.write [x, y+1]
       end
     end
     client.write ["done"]
   end
 
-  app.local do |client|
+  app.local(client_class) do |client|
     client.write [0, 0]
 
     2.times do
