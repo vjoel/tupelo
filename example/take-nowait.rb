1db13623500a8bb50b767e38238239922759ffbb
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-08-01 12:09:05 -0700

    add example/take-nowait.rb

---
+++ example/take-nowait.rb
@@ -0,0 +1,20 @@
+# Run this with --trace to see that, even in the FAIL case, take_nowait never
+# hangs waiting for a match. The "ready" tuple is just to keep the take
+# requests fairly close in time, increasing the chance of transaction failure.
+
+require 'tupelo/app'
+
+Tupelo.application do
+  20.times do
+    child do
+      read ["ready"]
+      r = take_nowait [1]
+      log "winner! result = #{r.inspect}" if r
+    end
+  end
+
+  local do
+    write [1]
+    write ["ready"]
+  end
+end
