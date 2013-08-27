e7d45abb4f7604c6a6be4aab120dedbb39a8d399
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-08-27 16:24:44 -0700

    remote example shows client ids for clarity

--- example/remote.rb
+++ example/remote.rb
@@ -1,7 +1,7 @@
 # simple use of remote to start process on remote host and connect it to
 # the tupelo app
 
-require 'tupelo/app'
+require 'tupelo/app/remote'
 
 host = ARGV.shift or abort "usage: #$0 <ssh-hostname>"
 
@@ -11,21 +11,21 @@ Tupelo.application(
   arcd_addr:  [:tcp, nil, 0]) do
 
   remote host: host do
-    write host: `hostname`.chomp, mode: "drb"
+    write host: `hostname`.chomp, mode: "drb", client: client_id
       # this actually returns local hostname, because the block executes
       # locally -- only the tupelo ops are remote. So this mode is really
       # only for examples and tests, not production.
   end
   
   remote host: host, eval: %{
-    write host: `hostname`.chomp, mode: "eval"
+    write host: `hostname`.chomp, mode: "eval", client: client_id
   }
   
 #  remote host, dir: "", run: "", args: []
   
   local do
     2.times do
-      log take host: nil, mode: nil
+      log take host: nil, mode: nil, client: nil
     end
   end
 end
