b98044d3f4dc1fb821e4818a2ccd6bca66ac5255
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-08-27 15:58:05 -0700

    remote works in eval mode

--- example/remote.rb
+++ example/remote.rb
@@ -5,18 +5,27 @@ require 'tupelo/app'
 
 host = ARGV.shift or abort "usage: #$0 <ssh-hostname>"
 
-Tupelo.application do
+Tupelo.application(
+  seqd_addr:  [:tcp, nil, 0], ## these should be defaults?
+  cseqd_addr: [:tcp, nil, 0],
+  arcd_addr:  [:tcp, nil, 0]) do
+
   remote host: host do
-    write host: `hostname`
+    write host: `hostname`.chomp, mode: "drb"
+      # this actually returns local hostname, because the block executes
+      # locally -- only the tupelo ops are remote. So this mode is really
+      # only for examples and tests, not production.
   end
   
-#  remote host, eval: %w{
-#    write {host: `hostname`}
-#  }
-#  
+  remote host: host, eval: %{
+    write host: `hostname`.chomp, mode: "eval"
+  }
+  
 #  remote host, dir: "", run: "", args: []
   
   local do
-    log read host: nil
+    2.times do
+      log take host: nil, mode: nil
+    end
   end
 end
--- lib/tupelo/app.rb
+++ lib/tupelo/app.rb
@@ -62,13 +62,52 @@ module Tupelo
       end
     end
 
-    def remote client_class = Client, passive: false, host: nil, **opts, &block
+    # Perform client operations on another host.
+    #
+    # There are three modes, corresponding to EasyServe#remote modes.
+    #
+    # There are some minor limitations compared to #child:
+    #
+    # In eval mode, the code string is always treated as in the arity 0 case
+    # of #child, in other words "DSL mode", in other words the self is the
+    # client.
+    #
+    # Unlike #child, there is no mode that returns a Client instance.
+    #
+    # #passive is not supported yet.
+    #
+    def remote client_class = Client,
+        client_lib: 'tupelo/client', host: nil, **opts
       require 'easy-serve/remote'
-      raise if passive ## not supported yet
+      raise if opts[:passive] ## not supported yet
+      snames = :seqd, :cseqd, :arcd
 
-      if block
-        ez.remote :seqd, :cseqd, :arcd, host: host,
-            **opts do |seqd, cseqd, arcd|
+      if opts[:eval]
+        ez.remote *snames, host: host, **opts, eval: %{
+          require #{client_lib.inspect}
+          
+          seqd, cseqd, arcd = *conns
+          client_class = Object.const_get(#{client_class.name.inspect})
+
+          begin
+            log.progname = "client <starting in \#{log.progname}>"
+            client = client_class.new(
+              seq: seqd, cseq: cseqd, arc: arcd, log: log)
+            client.start do
+              log.progname = "client \#{client.client_id}"
+            end
+            client.instance_eval #{opts[:eval].inspect}
+          ensure
+            client.stop if client
+          end
+        }
+
+      elsif opts[:file]
+        ###
+
+      elsif block_given?
+        block = Proc.new
+        ez.remote *snames, host: host, **opts do |seqd, cseqd, arcd|
           run_client client_class,
               seq: seqd, cseq: cseqd, arc: arcd, log: log do |client|
             if block.arity == 0
@@ -78,8 +117,9 @@ module Tupelo
             end
           end
         end
+
       else
-        raise
+        raise ArgumentError, "cannot select remote mode based on arguments"
       end
     end
 
