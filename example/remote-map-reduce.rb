12754d9c72f4204a5566706053b3c850b2b1be24
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-08-31 12:28:06 -0700

    added remote map-reduce example

---
+++ example/remote-map-reduce.rb
@@ -0,0 +1,27 @@
+require 'tupelo/app/remote'
+
+hosts = ARGV.shift or abort "usage: #$0 <ssh-hostname>,<ssh-hostname>,..."
+hosts = hosts.split(",")
+
+Tupelo.tcp_application do
+  hosts.each do |host|
+    remote host: host, passive: true, eval: %{
+      loop do
+        len = take([String])[0].size
+        write [len]
+      end
+    }
+  end
+
+  local do
+    input = [ ["We are going to"], ["calcula"], ["te the len"],
+              ["gth of this "], ["sentence."] ]
+    write *input
+    sum = 0
+    input.size.times do
+      sum += take([Numeric])[0]
+    end
+    log "sum = #{sum}, correct sum = #{input.flatten.join.size}"
+    sleep 2
+  end
+end
