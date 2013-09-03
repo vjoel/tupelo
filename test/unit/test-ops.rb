ee05117354be22774289f1eafa51725c7c6171c5
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-09-02 17:14:00 -0700

    test for transaction fail and retry

--- test/unit/test-ops.rb
+++ test/unit/test-ops.rb
@@ -217,6 +217,73 @@ class TestOps < Minitest::Test
     end
   end
   
+  def test_transaction_fail_retry
+    winner, loser = make_clients(2)
+    winner.write ["token", 1]; winner.update; loser.update
+    
+    run_count = 0
+    result = nil
+    lose_ops = Fiber.new do
+      result = loser.transaction do
+        run_count += 1
+        a = []
+        write ["test",  1]
+        a << (take  ["token", 1])
+        write ["test",  2]
+        a << (take  ["token", 2])
+        write ["test",  3]
+        a
+      end
+    end
+    
+    lose_ops.resume; loser.update # taking token 2
+    lose_ops.resume; loser.update # blocked on take token 2
+    assert_equal 1, run_count
+    
+    win_ops = Fiber.new do
+      winner.take ["token", 1]
+    end
+    
+    winner.update; win_ops.resume
+    winner.update; win_ops.resume
+    winner.update; win_ops.resume
+
+    loser.update; lose_ops.resume
+    assert_equal 2, run_count
+
+    winner.update
+    win_ops = Fiber.new do
+      winner.read_all [nil, nil]
+    end
+    win_ops.resume; winner.update
+    assert_equal [], win_ops.resume
+
+    win_ops = Fiber.new do
+      winner.write ["token", 2]
+    end
+    winner.update; win_ops.resume; winner.update
+    
+    loser.update
+    assert_equal :block, lose_ops.resume
+    
+    win_ops = Fiber.new do
+      winner.write ["token", 1]
+    end
+    winner.update; win_ops.resume; winner.update
+    
+    loser.update; lose_ops.resume
+    loser.update; lose_ops.resume
+    loser.update
+    assert_equal [["token", 1], ["token", 2]], lose_ops.resume
+    
+    winner.update
+    win_ops = Fiber.new do
+      winner.read_all [nil, nil]
+    end
+    win_ops.resume; winner.update
+    assert_equal [["test", 1], ["test", 2], ["test", 3]], win_ops.resume
+  end
+  
   def test_pulse
     t = ["c0"]
     cl = make_clients(2)
@@ -230,7 +297,4 @@ class TestOps < Minitest::Test
     reader = Fiber.new { cl[1].read_nowait [nil] }; reader.resume
     cl[1].update; assert_equal nil, reader.resume
   end
-
-  ## test failure
-  ## test optimistic
 end
