616352248f0b30d0c15043fcb38c64cc0b993372
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-12-11 01:09:44 -0800

    add address book example using rbtree

---
+++ example/subspaces/addr-book.rb
@@ -0,0 +1,139 @@
+# Example of attaching a data structure to a subspace. In this case, we
+# use an in-memory structure, a red-black tree, to maintain the tuples in
+# sorted order. (For a simpler example, with a hash instead of a tree,
+# see [memo example using subspaces](../multi-tier/memo2.rb). The process(es)
+# that manages the rbtree needs to subscribe to this subspace, so it can
+# apply writes to the rbtree.
+#
+# We also have subspaces for query commands and responses so that other clients
+# can access the sorted structure. The process(es) that host the rbtree also
+# subscribe to the command subspace (and write to, but not subscribe to, the
+# response subspace.) The process that query
+# do so by writing to the command subspace and subscribing to the response
+# subspace.
+#
+# This is kinda like redis, but the data is distributed, not stored on the same
+# process that is managing concurrency. Multiple replicas increase concurrency.
+#
+# Note that a subspace can be sharded to different clients, and different
+# clients can each use their own data structure for these tuples.
+
+require 'tupelo/app'
+require_relative 'sorted-set-space'
+
+N_REPLICAS = 1
+
+ab_tag = "my address book"
+cmd_tag = "#{ab_tag} commands"
+resp_tag = "#{ab_tag} responses"
+
+Tupelo.application do
+  local do
+    use_subspaces!
+
+    # Subspace for tuples belonging to the addr book.
+    define_subspace(
+      tag:          ab_tag,
+      template:     [
+        {value: ab_tag},
+        {type:  "string"},  # name
+        nil                 # address; can be any object
+      ]
+    )
+    
+    # Subspace for commands for fetch, delete, first, last, prev, next.
+    # We can't use #read and #take for fetch and delete because then the
+    # requesting client would have to subscribe to the ab_tag subspace.
+    define_subspace(
+      tag:          cmd_tag,
+      template:     [
+        {value: cmd_tag},
+        nil,                # request id, such as [client_id, uniq_id]
+        {type:  "string"},  # cmd name
+        {type:  "list"}     # arguments
+      ]
+    )
+
+    # Subspace for responses to commands. A response identifies the command
+    # it is responding to in two ways: by copying it and by an id. The
+    # former is so that another client can "spy" on one client's query
+    # responses, perhaps saving effort. The latter is to distinguish between
+    # iterations of the same command (first, first, ...).
+    define_subspace(
+      tag:          resp_tag,
+      template:     [
+        {value: resp_tag},
+        nil,                # in response to this request id
+        {type:  "string"},  # cmd name
+        {type:  "list"},    # arguments
+        nil,                # result of query -- type depends on command
+      ]
+    )
+  end
+
+  N_REPLICAS.times do |i|
+    # Inserts are just writes, which are handled by Worker and SortedSetSpace,
+    # so this child's app loop only needs to handle the special commands.
+    child tuplespace: [SortedSetSpace, ab_tag],
+          subscribe: [ab_tag, cmd_tag], passive: true do
+
+      log.progname = "replica ##{i}"
+
+      loop do
+        _, rqid, cmd, args = take(subspace cmd_tag)
+
+        case cmd
+        when "delete"                # handled by one replica
+          args.each do |name|
+            take [ab_tag, name, nil] # propagates to all replicas
+          end
+
+        when "fetch"
+          _, _, addr = read_nowait [ab_tag, args[0], nil] # addr might be nil
+          write [resp_tag, rqid, cmd, args, addr]
+
+        when "next", "prev", "first", "last"
+          _, name, addr = read_nowait SortedSetTemplate[ab_tag, cmd, *args]
+          write [resp_tag, rqid, cmd, args, [name, addr]]
+
+        else # maybe write an error message in a tuple
+          log.error "bad command: #{cmd}"
+        end
+      end
+    end
+  end
+  
+  child subscribe: resp_tag do
+    log.progname = "user agent"
+
+    counter = 0 # this is a bit hacky -- could use prev txn's global tick
+    next_rqid = proc { [client_id, counter+=1] }
+      # Protect this with a mutex if other threads need it.
+
+    # write some ab entries
+    write [ab_tag, "Eliza", "100 E St."]
+    write [ab_tag, "Alice", "100 A St."]
+    write [ab_tag, "Daisy", "100 D St."]
+    write [ab_tag, "Bob", "100 B St."]
+    write [ab_tag, "Charles", "100 C St."]
+
+    # make some queries
+    rqid = next_rqid.call
+    name = "Daisy"
+    write [cmd_tag, rqid, "fetch", [name]]
+    addr = take( [resp_tag, rqid, nil, nil, nil] ).last
+    log "found: #{name} => #{addr}"
+    
+    rqid = next_rqid.call
+    write [cmd_tag, rqid, "first", []]
+    name, addr = take( [resp_tag, rqid, nil, nil, nil] ).last
+    log "first entry: #{name} => #{addr}"
+    
+    5.times do
+      rqid = next_rqid.call
+      write [cmd_tag, rqid, "next", [name]]
+      name, addr = take( [resp_tag, rqid, nil, nil, nil] ).last
+      log( name ? "next entry: #{name} => #{addr}" : "no more entries" )
+    end
+  end
+end
---
+++ example/subspaces/sorted-set-space.rb
@@ -0,0 +1,161 @@
+require 'rbtree'
+
+## TODO
+##
+## generalize SortedSetSpace to accept params that indicate which fields
+##   are key and value
+
+class SortedSetTemplate
+  class << self
+    alias [] new
+  end
+  
+  # cmd can be "next", "prev", "first", "last"
+  # for next/prev, args is [key]
+  # for first/last, args is empty
+  def initialize tag, cmd, *args
+    @tag = tag
+    @cmd = cmd
+    @args = args
+  end
+  
+  def find_in tree, distinct_from: []
+    # for simplicity, ignore distinct_from
+    # -- we never take/read multiple keys in the tree
+    case @cmd
+    when "first"
+      k, v = tree.first
+      k && [@tag, k, v.first]
+    when "last"
+      tree.last
+      k && [@tag, k, v.last]
+    when "prev"
+      k = @args[0]
+      (k1,v1),(k2,v2) = tree.bound(tree.first[0], k).last(2)
+        ## Bad rbtree! This will be much less efficient than "next".
+      if k == k2
+        k1 && [@tag, k1, v1.last]
+      else
+        k2 && [@tag, k2, v2.last]
+      end
+      ## anomaly: can't iterate through multivalues
+    when "next"
+      k = @args[0]
+      (k1,v1),(k2,v2) = tree.bound(k, tree.last[0]).first(2)
+        ## Bad rbtree! There is no bounded search with < (rather than <=)
+      if k == k1
+        k2 && [@tag, k2, v2.first]
+      else
+        k1 && [@tag, k1, v1.first]
+      end
+    else
+      raise "bad command"
+    end
+  end
+end
+
+# A tuple store (in-memory) that is optimized for (key_string, object) pairs.
+# The object may be any serializable object (built up from numbers, booleans,
+# nil, strings, hashes and arrays).
+#
+# By default, multiple values per key are allowed. (This differs from a typical
+# key-value store, in wihch a given key_string may occur only once.)
+# It is up to the application to decide whether to enforce key uniqueness or
+# not (for example, by taking (k,...) before writing (k,v).
+#
+# This store should be used only by clients that subscribe to a subspace
+# that can be represented as triples (tag, key_string, value), where
+# the tag is a single literal value that is the same for all triples.
+# (See memo2.rb.)
+#
+# This store also manages command and meta tuples, which it keeps in an array,
+# just like the default Tuplespace class does.
+class SortedSetSpace
+  include Enumerable
+
+  attr_reader :tag, :tree, :metas
+
+  def initialize tag
+    @tag = tag
+    clear
+  end
+
+  def clear
+    @tree = RBTree.new{|t,k| t[k] = []}
+      # It's up to the application to enforce one entry per key.
+    @metas = []
+      # We are automatically subscribed to tupelo metadata (subspace defs), so
+      # we need to keep them somewhere. Also, the command tuples.
+  end
+
+  def each
+    tree.each do |k, vs|
+      vs.each do |v|
+        yield [tag, k, v]
+      end
+    end
+    metas.each do |tuple|
+      yield tuple
+    end
+  end
+
+  def insert tuple
+    if tuple.kind_of? Array and tuple.size == 3 and
+       tuple[0] == tag and tuple[1].kind_of? String
+
+      _, k, v = tuple
+      tree[k] << v
+
+    else
+      metas << tuple
+    end
+  end
+
+  def delete_once tuple
+    if tuple.kind_of? Array and tuple.size == 3 and
+       tuple[0] == tag and tuple[1].kind_of? String
+
+      _, k, v = tuple
+      if tree.key?(k) and tree[k].include? v
+        tree[k].delete v
+        tree.delete k if tree[k].empty?
+        true
+      else
+        false
+      end
+
+    else
+      if i=metas.index(tuple)
+        metas.delete_at i
+      end
+    end
+  end
+
+  def transaction inserts: [], deletes: [], tick: nil
+    deletes.each do |tuple|
+      delete_once tuple or raise "bug"
+    end
+
+    inserts.each do |tuple|
+      insert tuple.freeze ## should be deep_freeze
+    end
+  end
+
+  def find_distinct_matches_for templates
+    templates.inject([]) do |tuples, template|
+      tuples << find_match_for(template, distinct_from: tuples)
+    end
+  end
+
+  def find_match_for template, distinct_from: []
+    case template
+    when SortedSetTemplate
+      template.find_in tree, distinct_from: distinct_from
+    else
+      # fall back to linear search
+      find do |tuple|
+        template === tuple and not distinct_from.any? {|t| t.equal? tuple}
+      end
+    end
+  end
+end
