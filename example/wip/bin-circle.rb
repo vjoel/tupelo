b868a50c93670b62b92ae6ba0907795a49e67e19
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-01-06 22:00:31 -0800

    refactor consistent hashing

---
+++ example/wip/bin-circle.rb
@@ -0,0 +1,97 @@
+require 'digest/md5'
+require 'rbtree'
+
+# Used to determine which bin a given object is
+# associated with. For example: use it to look up which distributed cache
+# the object is stored in. Does not store or cache any data.
+# The state of a BinCircle instance depends entirely on two things:
+# the replications number (either provided as default_reps in BinCircle#new
+# or set per bin in #add_bin), and the current set of bins, which is managed
+# with #add_bin and #delete_bin.
+# Note: like rbtree, not thread-safe (for #delete and #show_bins).
+# https://en.wikipedia.org/wiki/Consistent_hashing
+class BinCircle
+  KEY_BITS = 30
+  KEY_MAX = 2**KEY_BITS - 1
+  DEFAULT_REPS = 10
+  
+  attr_accessor :default_reps
+
+  def initialize reps: DEFAULT_REPS
+    @tree = MultiRBTree.new # hashed_bin_id => bin_id
+    @default_reps = reps
+  end
+  
+  # +id+ should be an identifier for your bin, typically a number or string.
+  # Uses id.to_s, so +id+ should not contain a hash (which would not determine
+  # a unique string).
+  def add_bin id, reps: default_reps
+    rep_id = "#{id} 0000"
+    reps.times do |i|
+      key = key_for_string(rep_id)
+      @tree[key] = id
+      rep_id.succ!
+    end
+  end
+  
+  # This hashing fn is applied to both bin ids and object identifiers (in
+  # principle, these could be two different function with the same range of
+  # outputs.
+  def key_for_string str
+    Digest::MD5.hexdigest(str).to_i(16) & KEY_MAX
+  end
+  
+  def delete_bin id
+    @tree.delete_if {|k,v| v == id}
+  end
+  
+  # +id+ should be an identifier for your object, typically a number or string.
+  # Uses id.to_s, so +id+ should not contain a hash (which would not determine
+  # a unique string). It is not necessary for +id+ to be unique across objects.
+  def find_bin obj
+    find_bin_by_key(key_for_string(obj.to_s))
+  end
+  
+  def find_bin_by_key key
+    _, id = @tree.lower_bound(key) || @tree.first
+    id
+  end
+
+  def bin_size
+    Hash.new(0).tap do |bs|
+      bs[@tree.first[1]] = 2**30 - @tree.last[0] + @tree.first[0]
+      @tree.each_cons(2) do |(prev, _), (key, id)|
+        bs[id] += key - prev
+      end
+    end
+  end
+
+  # To help in tuning the replication counts, you can look at the
+  # distribution of bin sizes, plus mean and variance.
+  def show_bins
+    bs = bin_size
+    p bs
+
+    mean = bs.inject(0.0) {|sum, (id,n)| sum + n} / N_BINS
+    variance = bs.inject(0.0) {|sum_sqerr, (id,n)|
+      sum_sqerr + (n - mean)**2} / (N_BINS-1)
+    printf "mean : %14d\n" % mean
+    printf "stdev: %14d\n" % Math.sqrt(variance)
+  end
+end
+
+if __FILE__ == $0
+  N_BINS = 100
+  N_REPS = 20
+
+  circle = BinCircle.new reps: N_REPS
+
+  N_BINS.times do |id|
+    circle.add_bin id
+  end
+
+  circle.show_bins
+
+  p circle.find_bin "foo"
+  p circle.find_bin "bar"
+end
--- example/wip/consistent-hash.rb
+++ example/wip/consistent-hash.rb
@@ -1,46 +1,26 @@
 require 'tupelo/app'
-require 'digest/md5'
-require 'rbtree'
+require_relative 'bin-circle'
 
 N_BINS = 5
-N_REPLS = 30 # of each bin, to make distibution more uniform
-KEY_BITS = 30
-KEY_MAX = 2**KEY_BITS - 1
-
-def show_bins circle
-#  p circle
-  bin_size = Array.new(N_BINS, 0)
-  bin_size[circle.first[1]] = 2**30 - circle.last[0] + circle.first[0]
-  circle.each_cons(2) do |(prev, _), (key, i_bin)|
-    bin_size[i_bin] += key - prev
-  end
-  p bin_size
-
-  mean = bin_size.inject(0.0) {|sum, x| sum + x} / N_BINS
-  variance =
-    bin_size.inject(0.0) {|sum_sqerr, x| sum_sqerr + (x - mean)**2} / (N_BINS-1)
-  printf "mean : %14d\n" % mean
-  printf "stdev: %14d\n" % Math.sqrt(variance)
-end
+N_REPS = 30 # of each bin, to make distribution more uniform
 
 Tupelo.application do
   srand(12345)
-  circle = MultiRBTree.new
+  circle = BinCircle.new
 
-  N_BINS.times do |i_bin|
-    N_REPLS.times do |i_repl|
-      id = "bin #{i_bin}, repl #{i_repl}"
-      key = Digest::MD5.hexdigest(id).to_i(16) & KEY_MAX
-      circle[key] = i_bin
-    end
+  N_BINS.times do |id|
+    circle.add_bin id, reps: N_REPS
   end
 
-  show_bins(circle); exit
+  circle.show_bins; exit
 
-  N_BINS.times do |i_bin|
+  N_BINS.times do |id|
     child passive: true do
       # take things belonging to the repls of i_bin on circle
       # use custom tuplespace to pre-sort
+      loop do
+        #take proc {|key
+      end
     end
   end
 end
