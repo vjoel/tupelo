ba9c9647b6ecd77872cef0123a99025c510d39bb
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-01-16 20:48:49 -0800

    refactor

--- lib/tupelo/client.rb
+++ lib/tupelo/client.rb
@@ -4,6 +4,7 @@ module Tupelo
   class Client < Funl::Client
     require 'tupelo/client/worker'
     require 'tupelo/client/tuplespace'
+    require 'tupelo/client/subspace'
 
     include Api
 
@@ -59,38 +60,6 @@ module Tupelo
       else
         super().unknown *args
       end
-    end
-    
-    ## do these belong in API module?
-    def define_subspace metatuple
-      defaults = {__tupelo__: "subspace", addr: nil}
-      write_wait defaults.merge!(metatuple)
-    end
-    
-    # call this just once at start of first client (it's optional to
-    # preserve behavior of non-subspace-aware code)
-    def use_subspaces!
-      return if subspace(TUPELO_SUBSPACE_TAG)
-      define_subspace(
-        tag:          TUPELO_SUBSPACE_TAG,
-        template:     {
-          __tupelo__: {value: "subspace"},
-          tag:        nil,
-          addr:       nil,
-          template:   nil
-        }
-      )
-    end
-
-    def subspace tag
-      tag = tag.to_s
-      worker.subspaces.find {|sp| sp.tag == tag} or begin
-        if subscribed_tags.include? tag
-          read __tupelo__: "subspace", tag: tag, addr: nil, template: nil
-          worker.subspaces.find {|sp| sp.tag == tag}
-        end
-      end
-      ## this impl will not be safe with dynamic subspaces
     end
   end
 end
