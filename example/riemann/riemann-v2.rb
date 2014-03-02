fad76b0e5e27d29e045fe2dc4d4912bbc69f3fbf
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-03-01 22:00:51 -0800

    bring v2 up to v1 standard

--- example/riemann/riemann-v2.rb
+++ example/riemann/riemann-v2.rb
@@ -44,7 +44,7 @@ Tupelo.application do
   end
   
   # critical event alerter
-  child subscribe: "event", passive: true do ### tuplespace: bag?
+  child subscribe: "event", passive: true do ### tuplespace: hash?
     log.progname = "alerter"
     read Tupelo::Client::CRITICAL_EVENT do |event|
       log.error event
@@ -56,7 +56,15 @@ Tupelo.application do
     child subscribe: "event", passive: true do
       log.progname = "expiration debugger"
       read Tupelo::Client::EXPIRED_EVENT do |event|
-        log event
+        event_exp = event["time"] + event["ttl"]
+        delta = Time.now.to_f - event_exp
+        if delta > 0.1
+          log.warn "expired late by %6.4f seconds: #{event}" % delta
+        elsif delta < 0
+          log.warn "expired too soon: #{event}"
+        else
+          log "expired on time: #{event}"
+        end
       end
     end
   end
