e72738f8cd8ee20cd25728a564eef57dd3efc9db
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-03-02 22:10:45 -0800

    add minimal web server and client to example

--- example/riemann/riemann-v2.rb
+++ example/riemann/riemann-v2.rb
@@ -11,6 +11,26 @@
 
 abort "work in progress"
 
+# Run a web client
+fork do # No tupelo in this process.
+  sleep 1.0 # let some data arrive
+  require 'http'
+
+  url = 'http://localhost:4567'
+
+  print "trying server at #{url}"
+  begin
+    print "."
+    HTTP.get url
+  rescue Errno::ECONNREFUSED
+    sleep 0.2
+    retry
+  end
+
+  puts
+  puts HTTP.get "#{url}/read"
+end
+
 require 'tupelo/app'
 require_relative 'event-subspace'
 require_relative 'producer'
@@ -75,6 +95,33 @@ Tupelo.application do
     run_expirer_v2
     ### use rbtree
   end
+
+  # Web API using sinata to access the index of events.
+  child subscribe: "event", passive: true do |client|
+    require 'sinatra/base'
+
+    Class.new(Sinatra::Base).class_eval do
+      get '/read' do
+        host = params["host"] # nil is ok -- matches all hosts
+        resp = client.read(
+          host:         host,
+          service:      nil,
+          state:        nil,
+          time:         nil,
+          description:  nil,
+          tags:         nil,
+          metric:       nil,
+          ttl:          nil,
+          custom:       nil
+        )
+        resp.to_json + "\n"
+      end
+      ## need way to query by existence of tag
+
+      run!
+    end
+  end
   
-  ### Add sinatra app.
 end
+
+Process.waitall
